c2c0eab7dad11dc65109ac0867de7e6c5306eaef
NicolasYanB <nicolas07.yan@gmail.com>
2024-03-28 10:29:41 -0300

    update actions test

--- src/tests/actions.test.js
+++ src/tests/actions.test.js
@@ -1,9 +1,19 @@
 import { createRoom } from "../actions/create-room";
+import { ConnectionManager } from "../utils/connection-manager";
 
 describe('Actions Test Suite', () => {
 
     test('should create a room', async () => {
         const id = await createRoom();
         expect(typeof id).toBe('string');
+        const connectionManager = ConnectionManager.getConnectionManager();
+        expect(() => connectionManager.getConnection(id)).not.toThrow();
+        const conn = connectionManager.getConnection(id);
+        expect(conn).toHaveProperty('roomId');
+        expect(conn).toHaveProperty('ownerConnection');
+        expect(conn).toHaveProperty('visitorConnection');
+        expect(conn.roomId).toBe(id);
+        expect(conn.ownerConnection).toBeNull();
+        expect(conn.visitorConnection).toBeNull();
     });
 });
